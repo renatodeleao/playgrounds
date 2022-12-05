138e465e0735c278768e9359f1af49529fc58ecc
Renato de Leão <renatodeleao@gmail.com>
2022-12-05 15:12:51 +0000

    initial commit

---
+++ vue2.7/src/App.vue
@@ -0,0 +1,17 @@
+<script>
+import SimplebarVue from './demos/SimplebarVue.vue'
+
+export default {
+  name: 'App',
+  components: {
+    SimplebarVue
+  }
+}
+</script>
+
+<template>
+  <div>
+    <SimplebarVue />
+  </div>
+</template>
+
---
+++ vue3/src/demos/SimplebarVue.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    {{ { version } }}
+    <simplebar class="test" data-simplebar-auto-hide="false" @scroll="onScroll">
+      <div v-for="n in 10" :key="n">Example content</div>
+    </simplebar>
+  </div>
+</template>
+
+<script>
+import simplebar from 'simplebar-vue';
+import 'simplebar/dist/simplebar.min.css';
+import { version } from 'vue'
+
+export default {
+  name: 'app',
+  components: {
+    simplebar
+  },
+  data: () => ({
+    version
+  }),
+  methods: {
+    onScroll(e) {
+      // eslint-disable-next-line
+      console.log('onscroll', e)
+    }
+  }
+}
+</script>
+
+<style>
+.test {
+  height: 100px;
+}
+</style>
